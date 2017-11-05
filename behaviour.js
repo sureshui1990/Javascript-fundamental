6f20637fb721598c5504fa71cb76bb9f47153137
Suresh Bethanasamy <sureshui1990@gmail.com>
2017-11-05 19:20:29 +0530

    practised files added

--- behaviour.js
+++ behaviour.js
@@ -3,3 +3,12 @@ function SelectText () {
             input.focus();
             input.setSelectionRange(0,5);
 }
+
+var batsman = [
+    'sachin','sehwag','dhoni'
+];
+var bowlers = [
+    'nehra','bhuvaneswar','bhumra'
+];
+//batsman.reverse();
+document.write(batsman.concat(bowlers));
